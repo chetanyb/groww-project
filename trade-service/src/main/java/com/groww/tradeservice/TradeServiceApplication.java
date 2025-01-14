3eb88d84cc15aab980794239bc69119b7d24effc
Chetany Bhardwaj <chetanyb@groww.in>
2025-01-14 12:03:52 +0530

    added dockerfile and compose config for seamless single-command application deployment(NOTE: NOT RECOMMENDED FOR PROD CHECK DB CREDENTIALS)

--- trade-service/src/main/java/com/groww/tradeservice/TradeServiceApplication.java
+++ trade-service/src/main/java/com/groww/tradeservice/TradeServiceApplication.java
@@ -1,7 +1,10 @@
 package com.groww.tradeservice;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class TradeServiceApplication {
@@ -10,4 +13,11 @@ public class TradeServiceApplication {
 		SpringApplication.run(TradeServiceApplication.class, args);
 	}
 
+	@Bean
+	public NewTopic tradeConfirmationTopic() {
+		return TopicBuilder.name("trade-confirmation")
+				.partitions(1)
+				.replicas(1)
+				.build();
+	}
 }
