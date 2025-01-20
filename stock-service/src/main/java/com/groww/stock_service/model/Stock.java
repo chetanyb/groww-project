c0ddd40a588efebfc16579edb07b91eb06a19cb4
chethack <chetanyb@groww.in>
2025-01-20 11:48:01 +0530

    stock model, validation and other changes corresponding to bhav file

--- stock-service/src/main/java/com/groww/stock_service/model/Stock.java
+++ stock-service/src/main/java/com/groww/stock_service/model/Stock.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -14,10 +15,19 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Stock symbol cannot be null")
+    private String symbol;
+
+    @NotNull(message = "Stock name cannot be null")
     private String name;
     private Double openPrice;
     private Double closePrice;
     private Double highPrice;
     private Double lowPrice;
+    private Double lastTradedPrice;
     private Double settlementPrice;
+    private Double previousClosePrice;
+    private Long volumeTraded;
+    private String marketType;
+    private String securityType;
 }
--- stock-service/src/main/java/com/groww/stock_service/repository/StockRepository.java
+++ stock-service/src/main/java/com/groww/stock_service/repository/StockRepository.java
@@ -2,6 +2,13 @@ package com.groww.stock_service.repository;
 
 import com.groww.stock_service.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    // Custom query to find a stock by its symbol
+    Optional<Stock> findBySymbol(String symbol);
 }
